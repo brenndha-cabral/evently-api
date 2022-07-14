6f07373e2b89e2a1d61ca5e5b6d691b59a5aacf4
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-14 01:06:28 -0300

    feat: update server

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import 'express-async-errors';
+import 'dotenv/config';
+import express, { json } from 'express';
+import db from './database/config/config';
+
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(json());
+
+// app.use('/');
+
+// app.use('insert error middleware');
+
+app.listen(PORT, () => {
+  db.sync()
+    .then(() => console.log(`Connection was successfully established, listening at ${PORT}`))
+    .catch((error) => console.log('Unable to connect to the database: \n', error));
+});
