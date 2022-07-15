8c62f3172ed3bc4dc39628b34927c456d13cf1f1
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-15 18:35:48 -0300

    feat: create users migration

---
+++ src/database/migrations/20220714164818-create-users.js
@@ -0,0 +1,36 @@
+/* eslint-disable max-lines-per-function */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      adm: {
+        type: Sequelize.BOOLEAN,
+      },
+      created: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn('now'),
+      },
+    });
+  },
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
