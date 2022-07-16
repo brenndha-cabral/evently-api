8e911d9c04cf6ace7f57b1f148bbd6eeb8b045c3
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-07-15 22:26:29 -0300

    feat: update server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import 'express-async-errors';
 import 'dotenv/config';
 import express, { json } from 'express';
 import db from './database/config/config';
+import errorMiddleware from './middlewares/errorMiddleware';
+import routes from './routes';
 
 const { PORT } = process.env;
 
@@ -9,9 +11,9 @@ const app = express();
 
 app.use(json());
 
-// app.use('/');
+app.use('/', routes);
 
-// app.use('insert error middleware');
+app.use(errorMiddleware);
 
 db.sync().then(() => {
   app.listen(PORT, () => {
